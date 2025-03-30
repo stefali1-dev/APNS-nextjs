e0b311d68511224304c10375c7a79739b88977e0
stefali1-dev <stefanleustean56@gmail.com>
2025-03-30 21:34:59 +0300

    Initial commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+// app/contact/page.tsx
+
+export default function ContactPage() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>This is where you can put a contact form or your contact info.</p>
+      </div>
+    );
+  }
